7fde53078f8559988bd6d7a8e4ee2be52818ef68
Malshan Rathnayake <malshan.edu@gmail.com>
2024-10-12 09:50:17 +0530

    uri

--- career-pulse-flutter-app/career_pulse/lib/services/global_settings.dart
+++ career-pulse-flutter-app/career_pulse/lib/services/global_settings.dart
@@ -1,1 +1,1 @@
-const String baseApiUrl = 'https://b031-2407-c00-d001-e8c8-8811-8eac-32de-6b8f.ngrok-free.app/api';
+const String baseApiUrl = 'https://45b2-2407-c00-d001-e8c8-65a3-4a36-61f5-f54f.ngrok-free.app/api';
